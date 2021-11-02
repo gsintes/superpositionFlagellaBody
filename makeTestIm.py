acd5c6e2b097dadf2bf53132ed9de61123164f80
Guillaume Sintes <guillaumesintes@orange.fr>
2021-11-02 11:33:45 +0100

    Ellipse test

--- makeTestIm.py
+++ makeTestIm.py
@@ -19,14 +19,28 @@ def make_square_im(
                     im[i, j] = 1
     return im
 
+def make_ellipse_im(center: Tuple[int, int],
+    im_shape: Tuple[int, int],
+    a: int,
+    b: int)-> np.ndarray:
+    """Make a white ellispe in an image."""
+    x0 = center[0]
+    y0 = center[1]
+    im = np.zeros(im_shape)
+    for x in range(im_shape[0]):
+        for y in range(im_shape[1]):
+            r = ((x - x0) / a) ** 2 + ((y - y0) / b) ** 2
+            if r <= 1:
+                im[x, y] = 1
+    return im
+
+
 if __name__ == "__main__":
     im_shape = (100, 100)
     square_size = 10
-    image_init = make_square_im((0, 0), im_shape, square_size)
-    image_sec = make_square_im((10, 40), im_shape, square_size)
+    image_init = make_ellipse_im((50, 50), im_shape, 5, 3)
 
     plt.figure()
     plt.imshow(image_init, cmap="gray")
-    plt.figure()
-    plt.imshow(image_sec, cmap="gray")
+    
     plt.show(block=True)
