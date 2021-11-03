81314fb28cedbf87b4cb2846e333125ac3889dd1
Guillaume Sintes <guillaumesintes@orange.fr>
2021-11-03 11:36:11 +0100

    add angle to make ellipse

--- makeTestIm.py
+++ makeTestIm.py
@@ -21,15 +21,20 @@ def make_square_im(
 
 def make_ellipse_im(center: Tuple[int, int],
     im_shape: Tuple[int, int],
+    angle: float,
     a: int,
     b: int)-> np.ndarray:
     """Make a white ellispe in an image."""
     x0 = center[0]
     y0 = center[1]
+    x0p = np.cos(angle) * x0 + np.sin(angle) * y0
+    y0p = - np.sin(angle) * x0 + np.cos(angle) * x0
     im = np.zeros(im_shape)
     for x in range(im_shape[0]):
         for y in range(im_shape[1]):
-            r = ((x - x0) / a) ** 2 + ((y - y0) / b) ** 2
+            xp = np.cos(angle) * x + np.sin(angle) * y
+            yp = - np.sin(angle) * x + np.cos(angle) * y
+            r = ((xp - x0p) / a) ** 2 + ((yp - y0p) / b) ** 2
             if r <= 1:
                 im[x, y] = 1
     return im
@@ -38,7 +43,7 @@ def make_ellipse_im(center: Tuple[int, int],
 if __name__ == "__main__":
     im_shape = (100, 100)
     square_size = 10
-    image_init = make_ellipse_im((50, 50), im_shape, 5, 3)
+    image_init = make_ellipse_im((50, 50), im_shape, np.pi / 2, 40, 30)
 
     plt.figure()
     plt.imshow(image_init, cmap="gray")
