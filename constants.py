e49d118125c36206d1d890b61fa1780b2feb5b91
gsintes <guillaumesintes@orange.fr>
2022-07-13 15:17:42 +0200

    fix saving mire info

--- constants.py
+++ constants.py
@@ -1,13 +1,13 @@
 """Define the useful constants."""
 
-FOLDER_UP = "F:/Ficoll/Ficoll17%_20-11-05_1uLbactos_TRACKING/" 
-FOLDER = "F:/Ficoll/Ficoll17%_20-11-05_1uLbactos_TRACKING/2020-11-05_13h34m25s"
-# FOLDER = "/Volumes/GUILLAUME/film2colors/2021-11-23_17h00m13s"
+FOLDER_UP = "Z:/PhD/Flagelles/20-02-25_DSCG_12.63%_PVP_20umspacers_planaire_x63_" 
+FOLDER = "Z:/PhD/Flagelles/20-02-25_DSCG_12.63%_PVP_20umspacers_planaire_x63_/2020-02-25_19h41m55s"
+
 IM_SIZE = (1024, 1024)
 FPS = 80
 
-MIRE_INFO_PATH = "F:/Flagelles/20-02-26_mire_x63eau/2020-02-26_17h25m11s/mire_info.json"
-MIRE_PATH = "F:/Flagelles/20-02-26_mire_x63eau/2020-02-26_17h25m11s/Image0005748.tif"
+MIRE_INFO_PATH = "Z:/PhD/Flagelles/20-02-25_mire_x63air/2020-02-25_18h14m55s/mire_info.json"
+MIRE_PATH = "Z:/PhD/Flagelles/20-02-25_mire_x63air/2020-02-25_18h14m55s/Image0070652.tif"
 
 ANGLE_DATA_FILE = "angle_body_flagella.csv"
 
