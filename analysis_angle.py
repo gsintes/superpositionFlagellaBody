279e72bd318968b2707e4e62bd878d5ccab50437
Guillaume Sintes <guillaumesintes@orange.fr>
2022-08-03 10:51:02 +0200

    Refactor analysis

--- analysis_angle.py
+++ analysis_angle.py
@@ -11,158 +11,171 @@ import pandas as pd
 import constants
 from trackParsing import load_track_data
 
-def load_data(
-    folder: str = constants.FOLDER,
-    data_file: str = constants.ANGLE_DATA_FILE) -> Tuple[List[float], List[float]]:
-    """Load the angle data."""
-    data = np.genfromtxt(os.path.join(folder, data_file), delimiter=",")
-    time = data[:, 0]
-    angle = data[:, 1] 
-
-    return time, angle
-
-
 def fourier_transform(signal: List[float]) -> List[float]:
-    """
-    Calculate the Fourier transform of the signal.
+        """
+        Calculate the Fourier transform of the signal.
 
-    Input :
-    signal: List[float]
+        Input :
+        signal: List[float]
 
-    Output:
-    Fourier transform: List[float]
-    """
-    ft_signal = np.abs(np.fft.rfft(signal - np.mean(signal)))
-    ft_signal = list(ft_signal / max(ft_signal))
-    return ft_signal
+        Output:
+        Fourier transform: List[float]
+        """
+        ft_signal = np.abs(np.fft.rfft(signal - np.mean(signal)))
+        ft_signal = list(ft_signal / max(ft_signal))
+        return ft_signal
 
 def get_frequencies(signal: List[int], frame_rate: int = constants.FPS ) -> List[float]:
     """Give the frequency list associated with the Fourier transform"""
     freq = np.fft.rfftfreq(len(signal), d=1 / frame_rate)
     return freq
 
-def smooth_angle(angles: List[float], window_time: float) -> List[float]:
-    """Smooth the angle on a timescale window_time in secondes by FT cutting."""
-    freq = get_frequencies(angles)
-    ft_angle = np.fft.rfft(angles)
-    i = 0
-    f = freq[0]
-    while f < 1 / window_time:
-        i += 1
-        f = freq[i]
-    ft_angle[i: ] = 0
-    smooth_ang = list(np.fft.irfft(ft_angle))
-    if len(angles) % 2 == 1:
-        smooth_ang.append(0)
-    return smooth_ang
+class Analysis:
+    def __init__(self,
+        limits: Tuple[int, int],
+        folder: str = "",
+        data_file: str = constants.ANGLE_DATA_FILE,
+        angles: List[float]=[],
+        times: List[float]=[]
+        ) -> None:
+        """Load the angle data."""
+        self.folder = folder
+        if folder != "":
+            data = np.genfromtxt(os.path.join(folder, data_file), delimiter=",")
+            time = data[:, 0]
+            angle = data[:, 1] 
+        else:
+            time = times
+            angle = angle
+        self.limits = limits
+        lim_track0 = int(round(min(time) * constants.FPS)) + limits[0]
+        lim_track1 = int(round(min(time) * constants.FPS)) + limits[1]
+        self.track_data: pd.DataFrame = load_track_data()[lim_track0: lim_track1]
+        
+
+        self.times = time
+        self.angles = angle.copy()
+        self.times = self.times[limits[0]: limits[1]]
+        self.angles = angle[limits[0]: limits[1]]
+        self.cleaned_angles = self.angles.copy()
 
 
-def angle_shift(angles: List[float]) -> List[float]:
-    """Make the angle betweem -90 and 90 degrees."""
-    angles_c = angles.copy()
-    for i, ang in enumerate(angles_c):
-        if ang < - 90:
-            angles_c[i] = ang + 180
-        elif ang > 90:
-            angles_c[i] = ang - 180
-    return angles_c
+    def smooth_angle(self, window_time: float) -> None:
+        """Smooth the angle on a timescale window_time in secondes by FT cutting."""
+        freq = get_frequencies(self.cleaned_angles)
+        ft_angle = np.fft.rfft(self.cleaned_angles)
+        i = 0
+        f = freq[0]
+        while f < 1 / window_time:
+            i += 1
+            f = freq[i]
+        ft_angle[i: ] = 0
+        smooth_ang = list(np.fft.irfft(ft_angle))
+        if len(self.angles) % 2 == 1:
+            smooth_ang.append(0)
+        self.cleaned_angles = smooth_ang.copy()
 
 
-def detect_extrema(times: List[float], angles: List[float])-> np.ndarray:
-    """Detect the extrema of the angles."""
-    angles = np.array(angles)
-    differences = angles[1: ] - angles[:- 1]
-    extrema = []
-    for i, diff in enumerate(differences[: - 1]):
-        if diff * differences[i + 1] < 0:
-            extrema.append((times[i + 1], angles[i + 1]))
-    return np.array(extrema)
+    def angle_shift(self) -> None:
+        """Make the angle betweem -90 and 90 degrees."""
+        angles_c = self.cleaned_angles.copy()
+        for i, ang in enumerate(angles_c):
+            if ang < - 90:
+                angles_c[i] = ang + 180
+            elif ang > 90:
+                angles_c[i] = ang - 180
+        self.cleaned_angles = angles_c.copy()
+
+    def linear_interpolation(self) -> None:
+        """Make a linear interpolation for the missing data."""
+        angle_inter = list(self.cleaned_angles.copy())
+        times_inter = [k / constants.FPS for k in range(int(min(self.times) * constants.FPS), int(max(self.times) * constants.FPS) + 1)]
+        diff_times = self.times[1: ] - self.times[:len(self.times) -1]
+        for i, diff in enumerate(diff_times):
+            if diff > 1.5 / constants.FPS:
+                nb_missing = int(round(diff * constants.FPS)) - 1
+                slope = (np.mean(self.cleaned_angles[i + 1: i + 10]) - np.mean(self.cleaned_angles[i - 10: i])) / diff
+                for k in range(1, nb_missing + 1):
+                    ang = self.cleaned_angles[i] + k * slope / constants.FPS
+                    angle_inter.insert(i + k, ang)
+        self.times = times_inter.copy()
+        self.cleaned_angles = angle_inter
+
+    def clean_data(self, window_size: float) -> None:
+        """Prepare the data"""
+        self.angle_shift()
+        self.linear_interpolation()
+        self.smooth_angle(window_size)
+
+    def detect_extrema(self)-> None:
+        """Detect the extrema of the angles."""
+        angles = np.array(self.cleaned_angles)
+        differences = angles[1: ] - angles[:- 1]
+        extrema = []
+        for i, diff in enumerate(differences[: - 1]):
+            if diff * differences[i + 1] < 0:
+                extrema.append((self.times[i + 1], angles[i + 1]))
+        self.extrema = np.array(extrema)
 
 
-def get_amplitude(extrema: np.ndarray) -> Tuple[float, float]:
-    """Give the cone angle from the extrema."""
-    angles = extrema[:, 1]
-    diff = angles[1:] - angles[:-1]
-    return np.mean(np.abs(diff)) / 2, np.std(np.abs(diff)) / 2
+    def get_amplitude(self) -> None:
+        """Give the cone angle from the extrema."""
+        angles = self.extrema[:, 1]
+        diff = angles[1:] - angles[:-1]
+        self.amplitude = np.mean(np.abs(diff)) / 2
+        self.std_amplitude = np.std(np.abs(diff)) / 2
 
 
-def get_period(extrema: np.ndarray) -> Tuple[float, float]:
-    """Give the period from the extrema."""
-    times = extrema[:, 0]
-    diff = times[1:] - times[:-1]
-    return np.mean(np.abs(diff)) * 2, np.std(np.abs(diff)) * 2
+    def get_period(self) -> Tuple[float, float]:
+        """Give the period from the extrema."""
+        times = self.extrema[:, 0]
+        diff = times[1:] - times[:-1]
+        self.period = np.mean(np.abs(diff)) * 2
+        self.std_period = np.std(np.abs(diff)) * 2
 
+    def run_analysis(self, visualization: bool) -> pd.Series:
+        """Run the analysis on the section of the data."""
+        
 
-def linear_interpolation(times: List[float], angles: List[float]) -> Tuple[List[float], List[float]]:
-    """Make a linear interpolation for the missing data."""
-    angle_inter = list(angles.copy())
-    times_inter = [k / constants.FPS for k in range(int(min(times) * constants.FPS), int(max(times) * constants.FPS) + 1)]
-    diff_times = times[1: ] - times[:len(times) -1]
-    for i, diff in enumerate(diff_times):
-        if diff > 1.5 / constants.FPS:
-            nb_missing = int(round(diff * constants.FPS)) - 1
-            slope = (np.mean(angles[i + 1: i + 10]) - np.mean(angles[i - 10: i])) / diff
-            for k in range(1, nb_missing + 1):
-                ang = angles[i] + k * slope / constants.FPS
-                angle_inter.insert(i + k, ang)
-    return times_inter, angle_inter
+        self.clean_data(0.5)
+        self.detect_extrema()
+        freq = get_frequencies(self.cleaned_angles)
+        ft_angle = fourier_transform(self.cleaned_angles)
 
+        self.get_amplitude()
+        self.get_period()
+        self.mean_angle = np.mean(self.angles)
+        self.fourier_mode = freq[ft_angle.index(1)]
 
-def clean_data(time: List[float], angle: List[float], window_size: float) -> Tuple[List[float], List[float]]:
-    """Prepare the data"""
-    angle = angle_shift(angle)
-    time, angle = linear_interpolation(time, angle)
-    smooth_ang = smooth_angle(angle, window_size)
-    return  time, smooth_ang
+        data = pd.Series({
+            "Folder": self.folder,
+            "Limits": self.limits,
+            "Nb_extrema": len(self.extrema),
+            "Amplitude": self.amplitude,
+            "Std_amplitude": self.std_amplitude,
+            "Period": self.period,
+            "Std_period": self.std_period,
+            "Mean_angle": self.mean_angle,
+            "Fourier_mode": self.fourier_mode,
+            "Mean_vel": self.track_data["vel"].mean(),
+            "Std_vel": self.track_data["vel"].std()
+            })
 
+        if visualization:
+            plt.figure()
+            plt.plot(self.times, self.angles, "-")
+            plt.plot(self.extrema[:, 0], self.extrema[:, 1], "*r")
+            plt.xlabel("Time (in s)")
+            plt.ylabel("Angle (in degrees)")
+            plt.savefig(f"{self.folder}/angle.png")
 
-def run_analysis(folder: str, limits: Tuple[int, int], visualization: bool) -> pd.Series:
-    """Run the analysis on the section of the data."""
-    time, angle = load_data(folder)
-    lim_track0 = int(round(min(time) * constants.FPS)) + limits[0]
-    lim_track1 = int(round(min(time) * constants.FPS)) + limits[1]
-    track_data: pd.DataFrame = load_track_data()[lim_track0: lim_track1]
-    time = time[limits[0]: limits[1]]
-    angle = angle[limits[0]: limits[1]]
-    time, angle = clean_data(time, angle, 0.5)
-    extrema = detect_extrema(time, angle)
-    freq = get_frequencies(angle)
-    ft_angle = fourier_transform(angle)
-
-    amplitude, std_a = get_amplitude(extrema)
-    period, std_p = get_period(extrema)
-    mean_angle = np.mean(angle)
-    fourier_mode = freq[ft_angle.index(1)]
-
-    data = pd.Series({
-        "Folder": folder,
-        "Limits": limits,
-        "Nb_extrema": len(extrema),
-        "Amplitude": amplitude,
-        "Std_amplitude": std_a,
-        "Period": period,
-        "Std_period": std_p,
-        "Mean_angle": mean_angle,
-        "Fourier_mode": fourier_mode,
-        "Mean_vel": track_data["vel"].mean(),
-        "Std_vel": track_data["vel"].std()
-        })
-
-    if visualization:
-        plt.figure()
-        plt.plot(time, angle, "-")
-        plt.plot(extrema[:, 0], extrema[:, 1], "*r")
-        plt.xlabel("Time (in s)")
-        plt.ylabel("Angle (in degrees)")
-        plt.savefig(f"{folder}/angle.png")
-
-        plt.figure()
-        plt.plot(freq, ft_angle, label="smooth")
-        plt.xlabel("$f\ (in\ s^{-1})$")
-        plt.savefig(f"{folder}/fourier.png")
-        plt.close()
-        # plt.show(block=True)
-    return data
+            plt.figure()
+            plt.plot(freq, ft_angle, label="smooth")
+            plt.xlabel("$f\ (in\ s^{-1})$")
+            plt.savefig(f"{self.folder}/fourier.png")
+            plt.close()
+            # plt.show(block=True)
+        return data
 
 
 def get_limits(lim_lit: str) -> Tuple[int, int]:
@@ -175,10 +188,14 @@ def get_limits(lim_lit: str) -> Tuple[int, int]:
 
 if __name__ == "__main__":
     analysis_data = pd.read_csv(os.path.join(constants.FOLDER_UP, "wobbling_data.csv"))
+
+    data_list: List[pd.Series] = []
+    for _, info in analysis_data.iterrows():
+        analysis = Analysis(limits=get_limits(info["Limits"]), folder=info["Folder"])
+        data_list.append(analysis.run_analysis(visualization=False))
+
     data = pd.DataFrame()
-    data = pd.DataFrame(
-        [run_analysis(folder=info["Folder"], limits=get_limits(info["Limits"]), visualization=False) for _, info in analysis_data.iterrows()]
-        )
+    data = pd.DataFrame(data_list)
     data.to_csv(os.path.join(constants.FOLDER_UP, "wobbling_data.csv"))
     
     data["Count_freq"] = 1 / data["Period"]
--- plotting.py
+++ plotting.py
@@ -13,22 +13,51 @@ def load_data(file: str) -> pd.DataFrame:
 
 def plots(data: pd.DataFrame, filter: str) -> None:
     """Run all the plots"""
-    x = np.linspace(min(data["Fourier_mode"]), max(data["Fourier_mode"]))
 
+    dataLC = data[data["Fluid"] == "LC"]
+    dataFicoll = data[data["Fluid"] == "Ficoll"]
+
+    x = np.linspace(min(data["Fourier_mode"]), max(data["Fourier_mode"]))
     sns.relplot(x="Fourier_mode", y="Count_freq", hue="Fluid", style="Fluid", data=data)
     plt.plot(x, x, "k-")
     plt.plot(x, 0.75 * x, "k--")
     plt.plot(x, 1.25 * x, "k--")
     plt.savefig(os.path.join(fig_folder, f"frequency_check_{filter}.png"))
 
-    sns.relplot(x="Period", y="Mean_angle", hue="Fluid", style="Fluid", data=data)
+    plt.figure()
+    plt.errorbar(x=dataLC["Period"], xerr=dataLC["Std_period"], y=dataLC["Mean_angle"], linestyle="", marker="o", label="LC")
+    plt.errorbar(x=dataFicoll["Period"], xerr=dataFicoll["Std_period"], y=dataFicoll["Mean_angle"], linestyle="", marker="*", label="Ficoll")
+    plt.legend()
+    plt.xlabel("Period (s)")
+    plt.ylabel("Mean_angle ($\degree$)")
     plt.savefig(os.path.join(fig_folder, f"period_mean_angle_{filter}.png"))
 
-    sns.relplot(x="Mean_vel", y="Period", hue="Fluid", style="Fluid", data=data)
+    plt.figure()
+    plt.errorbar(x=dataLC["Mean_vel"], y=dataLC["Period"], yerr=dataLC["Std_period"], linestyle="", marker="o", label="LC")
+    plt.errorbar(x=dataFicoll["Mean_vel"], y=dataFicoll["Period"], yerr=dataFicoll["Std_period"], linestyle="", marker="*", label="Ficoll")
+    plt.legend()
+    plt.xlabel("Velocity (um/s)")
+    plt.ylabel("Period (s)")
     plt.savefig(os.path.join(fig_folder, f"period_vel_{filter}.png"))
 
-    sns.relplot(x="Mean_vel", y="Amplitude", hue="Fluid", style="Fluid", data=data)
+    plt.figure()
+    plt.errorbar(x=dataLC["Mean_vel"], y=dataLC["Amplitude"], yerr=dataLC["Std_amplitude"], linestyle="", marker="o", label="LC")
+    plt.errorbar(x=dataFicoll["Mean_vel"], y=dataFicoll["Amplitude"], yerr=dataFicoll["Std_amplitude"], linestyle="", marker="*", label="Ficoll")
+    plt.legend()
+    plt.xlabel("Velocity (um/s)")
+    plt.ylabel("Amplitude ($\degree$)")
     plt.savefig(os.path.join(fig_folder, f"amplitude_vel_{filter}.png"))
+
+    plt.figure()
+    plt.errorbar(y=dataLC["Mean_angle"], x=dataLC["Amplitude"], xerr=dataLC["Std_amplitude"], linestyle="", marker="o", label="LC")
+    plt.errorbar(y=dataFicoll["Mean_angle"], x=dataFicoll["Amplitude"], xerr=dataFicoll["Std_amplitude"], linestyle="", marker="*", label="Ficoll")
+    plt.legend()
+    plt.ylabel("Mean angle ($\degree$)")
+    plt.xlabel("Amplitude ($\degree$)")
+    x = np.linspace(min(data["Amplitude"]), max(data["Amplitude"]))
+    plt.plot(x, x, "k-")
+    plt.plot(x, -x, "k-")
+    plt.savefig(os.path.join(fig_folder, f"amplitude_mean_angle_{filter}.png"))
 
     sns.relplot(x="Period", y="Amplitude", hue="Fluid", style="Fluid", data=data)
     plt.savefig(os.path.join(fig_folder, f"amplitude_period_{filter}.png"))
@@ -51,10 +80,10 @@ if __name__=="__main__":
 
     for filter in filters:
         if filter == "filtered":
-            data1 = filtering_data(data)
+            dataLC = filtering_data(data)
         else: 
-            data1 = data  
-        plots(data1, filter)
+            dataLC = data  
+        plots(dataLC, filter)
 
 
     # plt.show(block=True)
