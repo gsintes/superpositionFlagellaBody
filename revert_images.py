1110ddae9981975e2384d52b923f10b0d23866e9
Guillaume Sintes <guillaumesintes@orange.fr>
2023-07-31 15:52:03 +0200

    Enable revert_image for folder of folder

--- revert_images.py
+++ revert_images.py
@@ -1,4 +1,4 @@
-"""Revert by  90deg right rotation all the images in a folder."""
+"""Revert by  90deg right rotation all the images."""
 import argparse
 import os
 
@@ -6,18 +6,33 @@ from PIL import Image
 
 def parse_arguments()-> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
+    parse_arguments("-r", "--recursive", help="""The folder is a folder of folder""", action="store_true")
     parser.add_argument("-f", "--folder", help="""Folder where to revert images?""")
     parser.add_argument("-v", "--verbose", help="Print the progression", action="store_true")
     return parser.parse_args()
 
-args = parse_arguments()
-folder = args.folder
+def revert_folder(folder: str, verbose: float) -> None:
+    """Revert the images in the folder."""
+    list_im = [os.path.join(folder, f) for f in os.listdir(folder) if (f.endswith(".tif") and not f.startswith("."))]
 
-list_im = [os.path.join(folder, f) for f in os.listdir(folder) if (f.endswith(".tif") and not f.startswith("."))]
+    for k, im_name in enumerate(list_im):
+        if args.verbose and k % 10 == 0: 
+            print(f"{100 * k/len(list_im):.2f}%")
+        im = Image.open(im_name)
+        out = im.rotate(-90, expand=True)
+        out.save(im_name)
 
-for k, im_name in enumerate(list_im):
-    if args.verbose and k % 10 == 0: 
-        print(f"{100 * k/len(list_im):.2f}%")
-    im = Image.open(im_name)
-    out = im.rotate(-90, expand=True)
-    out.save(im_name)
+def main(args: argparse.ArgumentParser)-> None:
+    folder: str = args.folder
+    verbose: bool = args.verbose
+    
+    if args.recursive:
+        for sub_folder in [os.path.join(folder, sf) for sf in os.listdir(folder) if os.path.isdir(os.path.join(folder, sf))]:
+            revert_folder(sub_folder, verbose)
+    else:
+        revert_folder(folder, verbose)
+    
+
+if __name__=="__main__":
+    args = parse_arguments()
+    main()
