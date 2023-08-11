9ef7eef81431c3b7943e4740b916db99192c8bd2
Guillaume Sintes <guillaumesintes@orange.fr>
2023-08-11 19:07:44 +0200

    Small fix

--- superimpose.py
+++ superimpose.py
@@ -11,40 +11,7 @@ import numpy as np
 
 import constants
 import superpositionTools as st
-
-class MireInfo:
-    def __init__(self, *args) -> None:
-        if len(args) == 2:
-            self.middle_line = args[0]
-            self.displacement = args[1]
-        if len(args) == 1:
-            arg = args[0]
-            if isinstance(arg, dict):
-                self.middle_line = arg["middle_line"]
-                self.displacement = arg["displacement"]
-            if isinstance(arg, str):
-                with open(arg) as f:
-                    data = json.load(f)
-                    self.middle_line = data["middle_line"]
-                    self.displacement = data["displacement"]
-
-    def delta_x(self) -> int:
-        """Return the displacement in x"""
-        return self.displacement[0]
-    
-    def delta_y(self) -> int:
-        """Return the displacement in y"""
-        return self.displacement[1]
-    
-    def save(self, file: str) -> None:
-        """Save the mire info in a json file."""
-        with open(file, "w", encoding="utf-8") as outfile:
-            outfile.write("")
-            json.dump(self.__dict__, outfile, indent=4)
-
-    def __repr__(self) -> str:
-        return f"Mire info:\n middle_line: {self.middle_line}\n Displacement: {self.displacement}"
-
+from mire_analysis import MireInfo
 
 def shift_image(
     image: np.ndarray,
@@ -126,4 +93,4 @@ if __name__ == "__main__":
     # list_dir = [f for f in os.listdir(parent_folder) if not f.startswith(".")]
     # for folder in list_dir[0: 1]:
         # folder_superposition(os.path.join(parent_folder, folder), "/Users/sintes/Desktop", mire_info)
-    folder_superposition(constants.FOLDEs, "/Users/sintes/Desktop", mire_info)
+    folder_superposition(constants.FOLDER, "/Users/sintes/Desktop", mire_info)
